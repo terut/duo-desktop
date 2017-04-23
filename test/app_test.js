0d778285048c78124dd920b9a3f281017675780e
terut <terut.dev+github@gmail.com>
2017-04-23 02:02:29 -0700

    Change test to es6
    mocha discourage us to use arrow function because of `this`, so we shouldn't use allow function.

--- test/app_test.js
+++ test/app_test.js
@@ -1,9 +1,10 @@
-var path = require('path')
-var Application = require('spectron').Application
-var assert = require('assert')
+import path from 'path'
+import { Application } from 'spectron'
+import assert from 'assert'
+
 var electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron')
 if (process.platform === 'win32') electronPath += '.cmd'
-var appPath = path.join(__dirname, '..')
+let appPath = path.join(__dirname, '..')
 
 describe('application launch', function() {
   this.timeout(10000)
